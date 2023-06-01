344522dd9284891ff798f29d24782d512ad3e71e
Haeun Kwon <s2118@e-mirim.hs.kr>
2023-06-01 10:01:42 +0900

    Shadowing 다시

--- ShadowingHidingOverridingPrac/Program.cs
+++ ShadowingHidingOverridingPrac/Program.cs
@@ -8,16 +8,31 @@ namespace ShadowingHidingOverridingPrac
 {
     internal class Program
     {
+        class Parent
+        {
+            public int Id { get; set; }
+        }
+
+        class Child : Parent
+        {
+
+        }
+
         // Shadowing 예제
         public static int number = 10; //같은 이름의 지역변수에 의해 가려짐(클래스 변수)
         public int number2 = 2; //같은 이름의 지역변수에 의해 가려짐(인스턴스 변수)
+
         static void Main(string[] args)
         {
             int number = 10;
-            int number2 = 3;
             Console.WriteLine(number); //지역변수 number 출력
-            Console.WriteLine(Program.number); //이렇게 접근은 가능
-            Console.WriteLine((new Program()).number2); //인스턴스 변수인 경우는 이렇게..
+            Console.WriteLine(Program.number); //이렇게 접근은 가능        
+        }
+        void doSometing()
+        {
+            int number2 = 3;
+            Console.WriteLine(number2);
+            Console.WriteLine((this.number2); //인스턴스 변수인 경우는 이렇게..
         }
     }
 }
